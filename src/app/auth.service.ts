1a5277ea4f04f90b1f8e39ad93473c4e31aa06b5
vaibhav dhiman <princedhiman70@gmail.com>
2021-10-18 11:44:52 +0530

    token login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
+import { Router } from '@angular/router';
+
+
 @Injectable()
 
 export class AuthService {
-    constructor(private http: Http) {}
-    BASE_URL = 'http://localhost:5544';
+    constructor(private http: Http, private router: Router) {}
 
-    register(user: any) {
+    BASE_URL = 'http://localhost:5544';
+    NAME_KEY = 'name';
+    TOKEN_KEY = 'token';
+
+    get name() {
+        return localStorage.getItem(this.NAME_KEY);
+    }
+
+    get isAuthenticated() {
+        return !!localStorage.getItem(this.TOKEN_KEY);
+    }
+
+    register(user) {
         delete user.confirmPassword;
-        return this.http.post(this.BASE_URL + '/register', user).subscribe();
+        this.http.post(this.BASE_URL + '/register', user).subscribe(res => {
+                localStorage.setItem(this.TOKEN_KEY, res.json().token)
+                localStorage.setItem(this.NAME_KEY, res.json().firstName)
+                this.router.navigate(['/']);
+        });
     }
 }
--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { AuthService } from './auth.service'
+
 
 @Component({
     selector: 'nav',
@@ -6,10 +8,12 @@ import { Component } from '@angular/core'
         <md-toolbar color="primary">
             <button md-button routerLink="/" >Message Board</button>
             <button md-button routerLink="/messages" >Messages</button>
+            <span style="flex: 1 1 auto"></span>
             <button md-button routerLink="/register" >Register</button>
+            <button *ngIf = "authService.isAuthenticated" md-button>Welcome {{ authService.name }}</button>
         </md-toolbar>
     `
 })
 export class NavComponent {
-    constructor() {}
+    constructor(private authService: AuthService) {}
 }
